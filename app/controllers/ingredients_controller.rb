d15f1a93440c840cab447a9836a42a86d8fcb0fa
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-11-23 12:43:15 +0300

    complete functional has_and_belongs_to_many example

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,45 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create 
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.save
+
+    redirect_to  @ingredient
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+
+    redirect_to  @ingredient
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+
+    redirect_to ingredients_path
+  end
+
+  private
+
+    def ingredient_params
+        params.require(:ingredient).permit(:name, :recipe_ids => [])
+    end
+end
